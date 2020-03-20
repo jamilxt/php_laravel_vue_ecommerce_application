8b32d26a08d2fb900d0bfbd7ae4638d59c0b1d50
Jamilur Rahman <jamilxt@gmail.com>
2020-03-21 02:19:40 +0600

    Some Fixes to Database

--- app/Models/ProductAttribute.php
+++ app/Models/ProductAttribute.php
@@ -18,7 +18,7 @@ class ProductAttribute extends Model
     /**
      * @var array
      */
-    protected $fillable = ['product_id', 'quantity', 'price'];
+    protected $fillable = ['product_id', 'quantity', 'price', 'attribute_id', 'value'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -34,5 +34,10 @@ class ProductAttribute extends Model
     public function attributesValues()
     {
         return $this->belongsToMany(AttributeValue::class);
+    }
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
     }
 }
