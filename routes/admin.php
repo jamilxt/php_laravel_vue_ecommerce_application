d48e89019be525fe74ef5f2835ecb81c18a9aae2
Jamilur Rahman <jamilxt@gmail.com>
2020-03-21 02:27:16 +0600

    Adding Required Routes

--- routes/admin.php
+++ routes/admin.php
@@ -65,6 +65,18 @@ Route::group(['prefix' => 'admin'], function () {
             Route::post('images/upload', 'Admin\ProductImageController@upload')->name('admin.products.images.upload');
             Route::get('images/{id}/delete', 'Admin\ProductImageController@delete')->name('admin.products.images.delete');
 
+
+            // Load attributes on the page load
+            Route::get('attributes/load', 'Admin\ProductAttributeController@loadAttributes');
+            // Load product attribute on the page load
+            Route::post('attributes', 'Admin\ProductAttributeController@productAttributes');
+            // Load option values for a attribute
+            Route::post('attributes/values', 'Admin\ProductAttributeController@loadValues');
+            // Add product attribute to the current product
+            Route::post('attributes/add', 'Admin\ProductAttributeController@addAttribute');
+            // Delete product attribute from the current product
+            Route::post('attributes/delete', 'Admin\ProductAttributeController@deleteAttribute');
+
         });
     });
 
