a34f6fe903adf152a458c668f954e0bc5307935e
Jamilur Rahman <jamilxt@gmail.com>
2020-03-23 19:18:43 +0600

    add Product Details Page

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -5,20 +5,30 @@ namespace App\Http\Controllers\Site;
 use App\Contracts\ProductContract;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Contracts\AttributeContract;
 
 class ProductController extends Controller
 {
     protected $productRepository;
+    protected $attributeRepository;
 
-    public function __construct(ProductContract $productRepository)
+    public function __construct(ProductContract $productRepository, AttributeContract $attributeRepository)
     {
-        $this->productRepository = $productRepository;
+        $this->productRepository   = $productRepository;
+        $this->attributeRepository = $attributeRepository;
     }
 
     public function show($slug)
     {
-        $product = $this->productRepository->findProductBySlug($slug);
+        $product    = $this->productRepository->findProductBySlug($slug);
+        $attributes = $this->attributeRepository->listAttributes();
 
-        dd($product);
+//        dd($product);
+        return view('site.pages.product', compact('product', 'attributes'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        dd($request->all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::view('/', 'site.pages.homepage');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
 
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
+
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
