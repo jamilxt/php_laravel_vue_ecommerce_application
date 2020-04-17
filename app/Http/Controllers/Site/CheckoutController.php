dc5ca7828d92b3b954e7d6931f83eb9017c6ef5e
Jamilur Rahman <jamilxt@gmail.com>
2020-04-17 23:23:58 +0600

    add payment processing

--- app/Http/Controllers/Site/CheckoutController.php
+++ app/Http/Controllers/Site/CheckoutController.php
@@ -4,26 +4,57 @@ namespace App\Http\Controllers\Site;
 
 use App\Contracts\OrderContract;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Darryldecode\Cart\Cart;
 use Illuminate\Http\Request;
+use App\Services\PayPalService;
 
 class CheckoutController extends Controller
 {
     protected $orderRepository;
+    protected $payPal;
 
-    public function __construct(OrderContract $orderRepository)
+    public function __construct(OrderContract $orderRepository, PayPalService $payPal)
     {
         $this->orderRepository = $orderRepository;
+        $this->payPal          = $payPal;
     }
 
-    public function getCheckout() {
+    public function getCheckout()
+    {
         return view('site.pages.checkout');
     }
 
-    public function placeOrder(Request $request) {
+    public function placeOrder(Request $request)
+    {
         // Before storing the order we should implement the
         // request validation which I leave it to you
         $order = $this->orderRepository->storeOrderDetails($request->all());
 
-        dd($order);
+        // You can add more control here to handle if the order
+        // is not stored properly
+
+        if ($order) {
+            $this->payPal->processPayment($order);
+        }
+
+        return redirect()->back()->with('message', 'Order not placed');
+    }
+
+    public function complete(Request $request)
+    {
+        $paymentId = $request->input('paymentId');
+        $payerId   = $request->input('PayerID'); // jamilxt - maybe payerId
+
+        $status = $this->payPal->completePayment($paymentId, $payerId);
+
+        $order                 = Order::where('order_number', $status['invoiceId'])->first();
+        $order->status         = 'processing';
+        $order->payment_status = 1;
+        $order->payment_method = 'PayPal -' . $status['salesId'];
+        $order->save();
+
+        Cart::clear();
+        return view('site.pages.success', compact('order'));
     }
 }
--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -55,6 +55,7 @@
                                         {{ Auth::user()->full_name }} <span class="caret"></span>
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('account.orders') }}">Orders</a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
